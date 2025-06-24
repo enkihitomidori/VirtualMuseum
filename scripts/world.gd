d565d263e7e00c4c3c7337b81933349637321c15
Jani Pekola <jani.pekola@gmail.com>
2025-06-24 13:11:32 +0300

    mainmenu ui + settings

--- scripts/world.gd
+++ scripts/world.gd
@@ -8,7 +8,7 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("world.gd init")
-	scaleToMonitor()
+	#scaleToMonitor()
 	
 	# load default scene
 	#if defaultScene:
