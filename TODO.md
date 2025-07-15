8bc6e57122add411a067af5e9e9b6390abfba57d
Jani Pekola <jani.pekola@gmail.com>
2025-07-15 13:23:40 +0300

    artObject 2.0 auto frame gen + bug tracker with TODO.md

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODO
+
+## Bugs:
+- Paintings dont cast shadows with artObject 2.0
+- 
--- scripts/art_object_2.gd
+++ scripts/art_object_2.gd
@@ -8,31 +8,127 @@ extends Node3D
 		#targetModel = value
 		#_update_model()
 
-@export_file("*.jpg", "*.png") var targetPainting: String:
+@export_file("*.jpg", "*.png") var Artwork: String:
 	set(value):
-		targetPainting = value
+		Artwork = value
 		_update_texture()
+
+@export var EnableFrame: bool = false:
+	set(value):
+		EnableFrame = value
+		if EnableFrame:
+			generate_frame()
+		else:
+			clearFrame()
+
+@export var FrameDepth: float = 0.2:
+	set(value):
+		if value >= 0:
+			FrameDepth = value
+			if EnableFrame:
+				generate_frame()
+			
+@export var FrameWidth: float = 0.2:
+	set(value):
+		if value >= 0:
+			FrameWidth = value
+			if EnableFrame:
+				generate_frame()
+				
+@export var FrameColor: Color = Color.WHITE:
+	set(value):
+		FrameColor = value
+		generate_frame()
 
 @onready var Painting: Sprite3D = $Painting
 @onready var Model: Node3D = $Model
+@onready var Frame: Node3D = $Frame
 
 func _ready() -> void:
 	_update_texture()
 	#_update_model()
+	if EnableFrame:
+		generate_frame()
 
 func _update_texture() -> void:
-	if not Painting or not targetPainting:
+	if !Painting or !Artwork:
 		return
 
-	var text = load(targetPainting)
+	var text = load(Artwork)
 	if text:
 		Painting.texture = text
+		await get_tree().process_frame # wait for texture
+		
+		if Frame and EnableFrame:
+			generate_frame()
+
+func clearFrame():
+	if !Frame:
+		push_error("Frame null?")
+		return
+		
+	for child in Frame.get_children():
+		child.queue_free()
+		
+	return
+
+
+func generate_frame():
+	if !Painting or !Painting.texture:
+		push_error("Painting texture null")
+		return
+		
+	# clear
+	clearFrame()
+	
+	var size = Painting.texture.get_size() * Painting.pixel_size
+	size.x *= Painting.scale.x
+	size.y *= Painting.scale.y
+	
+	var half_w = size.x / 2.0
+	var half_h = size.y / 2.0
+
+# Define frame bars
+	var frameDefs = [
+		{ # Top
+			"size": Vector3(size.x + FrameWidth * 2, FrameWidth, FrameDepth),
+			"pos": Vector3(0, half_h + FrameWidth / 2.0, 0)
+		},
+		{ # Bottom
+			"size": Vector3(size.x + FrameWidth * 2, FrameWidth, FrameDepth),
+			"pos": Vector3(0, -half_h - FrameWidth / 2.0, 0)
+		},
+		{ # Left
+			"size": Vector3(FrameWidth, size.y, FrameDepth),
+			"pos": Vector3(-half_w - FrameWidth / 2.0, 0, 0)
+		},
+		{ # Right
+			"size": Vector3(FrameWidth, size.y, FrameDepth),
+			"pos": Vector3(half_w + FrameWidth / 2.0, 0, 0)
+		}
+	]
+
+	# generate frame
+	for def in frameDefs:
+		var mesh_instance = MeshInstance3D.new()
+		var box = BoxMesh.new()
+		box.size = def["size"]
+		
+		# set color
+		var material = StandardMaterial3D.new()
+		if !FrameColor: FrameColor = Color.WHITE
+		material.albedo_color = FrameColor
+		box.material = material
+		
+		mesh_instance.mesh = box
+		mesh_instance.translate(def["pos"])
+		Frame.add_child(mesh_instance)
 
 # wip and NOT tested
 #func _update_model() -> void:
-	#if not Model or not targetModel:
+	#if !Model or !targetModel:
 		#return
-		
+		#
 	#var modelScene = load(targetModel)
 	#if modelScene is PackedScene:
 		#for child in Model.get_children():
